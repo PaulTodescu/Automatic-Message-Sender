15ea0b0652f0bff4fd1b89fd19e274887e715268
Todescu Paul <paul.todescu00@e-uvt.ro>
2021-04-20 19:52:10 +0300

    added password file and validation

--- app/main/views.py
+++ app/main/views.py
@@ -1,9 +1,27 @@
+from xml.dom import minidom
+
+from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 import csv
+from django.views.generic import TemplateView
+from django.contrib.auth.views import LoginView
 
 from list.models import List
+from .forms import LoginForm
+from django.contrib import messages
+import base64
+
+
+# def encrypt_alg1(text):
+#     encoded_data = base64.b64encode(text)
+#     return encoded_data
+
+
+def decrypt_alg1(text):
+    decoded_data = base64.b64decode(text)
+    return decoded_data
 
 
 @login_required
@@ -11,8 +29,62 @@ def home(request):
     return render(request, "index.html")
 
 
-def login(request):
-    return render(request, "login.html")
+class LoginPage(LoginView):
+
+    template_name = 'login.html'
+
+    def get(self, request, *args, **kwargs):
+        loginForm = LoginForm()
+
+        context = {
+            'form': loginForm
+        }
+
+        return render(request, self.template_name, context)
+
+    def post(self, request, *args, **kwargs):
+        loginForm = LoginForm(request.POST) # create login form
+
+        text = None
+
+        if loginForm.is_valid():
+            text = loginForm.cleaned_data['password'] # get the password from the form
+
+        user = None
+
+        confirmation = None
+
+        xmldoc = minidom.parse('main/password.xml') # get the password form xml
+
+        value = "".join(
+            el.nodeValue for el in xmldoc.getElementsByTagName('value')[0].childNodes if el.nodeType == el.TEXT_NODE)
+
+        alg = "".join(
+            el.nodeValue for el in xmldoc.getElementsByTagName('algorithm')[0].childNodes if
+            el.nodeType == el.TEXT_NODE)
+
+        password = None
+
+        # check which encryption algorithm is used
+        if alg == "a1":
+            password = decrypt_alg1(value).decode() # decrypt the password from xml
+
+        # login user
+        if text == password:
+            user = authenticate(request, username='myname',password=text)
+            confirmation = "logged in"
+        else:
+            confirmation = "incorrect password"
+
+        if user is not None:
+            login(request, user)
+
+        context = {
+            'form': loginForm,
+            'confirmation': confirmation
+        }
+
+        return render(request, self.template_name, context)
 
 
 def send_msg(request):
